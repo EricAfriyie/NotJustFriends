e4967954ca49794889f6834cff09ea63d0d8551b
Eric Afriyie <kae.afriyie@gmail.com>
2022-09-22 21:24:58 +0000

    started post screen

--- App.js
+++ App.js
@@ -1,10 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+
+const post = {
+  id: "p1",
+  createdAt: "19 m",
+  User: {
+    id: "u1",
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
+    name: "Kwesi Afriyie",
+  },
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  image: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg",
+  numberOfLikes: 11,
+  numberOfShares: 2,
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={styles.post}>
+        {/*header*/} 
+        <View style={styles.header}>
+          <Image source={{uri:post.image}} 
+          style={styles.profileImage}/>
+
+          <View>
+            <Text style={styles.name}>{post.User.name}</Text>
+            <Text style={styles.subtitle}>{post.createdAt}</Text>
+          </View>
+
+          <View>
+          <Entypo name="dots-three-horizontal" size={24} color="black" />
+          </View>
+
+        </View>
+        {/*body*/}
+        
+        {/*footer*/}
+        <View style={styles.footer}>
+
+        </View>
+
+      </View>
+     
+
+
+
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +63,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  profileImage:{
+    width:50,
+    height:50,
+
   },
 });
